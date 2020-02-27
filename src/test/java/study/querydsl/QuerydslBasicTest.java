dd37b4df9229a5e6ec8e3c43dd1c6a6df373c165
moving-line <movinglinecheck@gmail.com>
2020-02-27 23:24:29 +0900

    Add Constant & Concat
    - 상수 사용, 문자더하기 케이스 추가

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -413,6 +414,32 @@ public class QuerydslBasicTest {
                         .when(member.age.between(21, 30)).then("21~30살")
                         .otherwise("기타")
                 )
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+
+    // 더해서 "username_age" 형태
+    @Test
+    void constantConcat() {
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue())) // enum 쓸 때도 stringValue()
                 .from(member)
                 .fetch();
 
